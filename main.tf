e4ca2d4c07918b3e7dfa22aee4eb8f6aa6b6062d
mujahed85 <mujahed.mca@gmail.com>
2024-03-03 20:05:14 +0530

    started working on version 2 task02.tf might have error in both check before running

--- main.tf
+++ main.tf
@@ -1,129 +1,216 @@
-# 01. Create AWS Provider
+#provider info
 provider "aws" {
-    region   = "us-east-2"
-    profile  = "nubeera"  
+  region   = "us-east-2"
+  profile  = "nubeera"
 }
 
-# 02. Create S3 Bucket, Object Lock, CORS Rules
-resource "aws_s3_bucket" "bucketfrancis" {
-  bucket = "bucketfrancis"
-  acl = "public-read"
+#creating s3
+resource "aws_s3_bucket" "bucketmujahedh" {
+  bucket = "bucketmujahedh"
   force_destroy = true
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-  }
-
+  object_lock_enabled = true
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT","POST"]
+    allowed_methods = ["PUT", "POST"]
     allowed_origins = ["https://mujahedh.com"]
-    expose_headers = ["ETag"]
-    max_age_seconds = 4000
+    expose_headers  = ["ETag"]
+    max_age_seconds  = 4000
   }
 }
 
-# 03. Execute/Clone Locally Project
+resource "aws_s3_bucket_acl" "bucket_mujahedh_acl_configuration" {
+  depends_on = [
+    aws_s3_bucket.bucketmujahedh,
+  ]
+  bucket = aws_s3_bucket.bucketmujahedh.id
+  acl    = "private"
+}
+
+#to cloning data to local
 resource "null_resource" "cloning-data" {
-    depends_on = ["aws_s3_bucket.bucketfrancis"]
-    provisioner "local-exec" {
-        command = "git clone https://github.com/NubeEra-Samples/static-resources.git mybadges"    
-    }  
+  depends_on = ["aws_s3_bucket.bucketmujahedh"]
+  provisioner "local-exec" {
+      command = "git clone https://github.com/NubeEra-Samples/static-resources.git mybadges"
+  }
 }
 
-# 04. Upload obj --> CloudFront
+#upload
 resource "aws_s3_bucket_object" "obj" {
-    depends_on = [ aws_s3_bucket.bucketfrancis, null_resource.cloning-data ]
-    bucket = "bucketfrancis"
-    key = "mastry_badge.jpg"
-    source = "mybadges/mastry_badge.jpg"
-    acl = "public-read"
+  depends_on = [aws_s3_bucket.bucketmujahedh,null_resource.cloning-data]
+  bucket = "bucketmujahedh"
+  key = "mastry_badge.jpg"
+  source = "mybadges/mastry_badge.jpg"
+  acl = "private"
 }
 
-# 05. Create EC2 Instance( Execute SSH based Installing Apache2)
+# creating cloudfront s3 distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name =  "${aws_s3_bucket.bucketmujahedh.bucket_regional_domain_name}"
+    origin_id   =  "S3-${aws_s3_bucket.bucketmujahedh.bucket}"
+    custom_origin_config {
+       http_port = 80
+       https_port = 443
+       origin_protocol_policy = "match-viewer"
+       origin_ssl_protocols = ["TLSv1","TLSv1.1","TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "mujahedhussaini"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.bucketmujahedh.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.bucketmujahedh.bucket}"
+
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "cloud_production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+#creating ec2 instance as webserver
 resource "aws_instance" "web01" {
-  ami = "ami-05fb0b8c1424f266b"
+  ami           = "ami-05fb0b8c1424f266b"
   instance_type = "t2.micro"
-  key_name = "mujahed"
+  key_name      = "mujahed"
   vpc_security_group_ids = ["sg-442f3733","sg-00c1ce33d5587a75e"]
   connection {
     type = "ssh"
     user = "ubuntu"
     private_key = file("c:/Users/info/.ssh/mujahed.pem")
-    host = aws_instance.web01.public_ip
-  }  
-  tags = {
-        Name = "web01"
-  }
+    host    = aws_instance.web01.public_ip
+  } 
+
   provisioner "remote-exec" {
     inline = [
       "sudo apt install apache2 php git -y",
       "sudo systemctl restart apache2",
-      "sudo systemctl enable apache2"
-    ]    
+      "sudo systemctl enable apache2",
+    ]
   }
-}
-
-# 06.1 Create EBS Volume
-resource "aws_ebs_volume" "myVolume" {
-  availability_zone = aws_instance.web01.availability_zone
-  size = 1
-  encrypted = true
   tags = {
-    Name = "MyEBSVolume"
+    Name = "MYFirstTerraOS"
   }
 }
 
-# 06.2 Display Volume Id
-output "myout_vol_id" {
-  value = aws_ebs_volume.myVolume.id  
+#creating volume
+resource "aws_ebs_volume" "myVolume" {
+	availability_zone = aws_instance.web01.availability_zone
+	size = 1
+	encrypted = "true" 
+	tags = {
+		Name = "MyEBSVolume"
+	}
 }
 
-# 06.3 Attach Volume to EC2 Instance
+output "myout_vol_id" {
+	value = aws_ebs_volume.myVolume.id
+}
+
+
+#attaching volume
 resource "aws_volume_attachment" "ebs_attached" {
   device_name = "/dev/sde"
-  volume_id = aws_ebs_volume.myVolume.id
+  volume_id   = aws_ebs_volume.myVolume.id
   instance_id = aws_instance.web01.id
-  force_detach =  true
+  force_detach = true
 }
 
-# 06.3  Convert EBS Volume into File System ext4
-resource "null_resource" "nullremote3" {
-  depends_on = [ aws_volume_attachment.ebs_attached ]
-  #SSH Connect
+#creating a text file that store the public ip 
+resource "null_resource" "nullrsclo01"  {
+	provisioner "local-exec" {
+	    command = "echo  ${aws_instance.web01.public_ip} > publicip.txt"
+  }
+}
+
+#creating a null resource to provision the partitions
+resource "null_resource" "nullremote3"  {
+  depends_on = [
+      aws_volume_attachment.ebs_attached,
+  ]
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type     = "ssh"
+    user     = "ubuntu"
     private_key = file("c:/Users/info/.ssh/mujahed.pem")
-    host = aws_instance.web01.public_ip
+    host     = aws_instance.web01.public_ip
   }
-  #Execute Commands
   provisioner "remote-exec" {
-    inline = [ 
-      "sudo mkfs.ext4 /dev/xvde",
-      "sudo mount /dev/xvde /var/www/html",
+    inline = [
+      "sudo mkfs.ext4  /dev/xvde",
+      "sudo mount  /dev/xvde  /var/www/html",
       "sudo rm -rf /var/www/html/*",
-      "sudo git clone git https://github.com/NubeEra-Samples/TF-InfraDeploy.git /var/www/html/",
-     ]
-    
+      "sudo git clone https://github.com/NubeEra-Temps/webserberProvisioning.git /var/www/html/"
+    ]
   }
 }
 
-# 07.1 Create Snapshot
-resource "aws_ebs_snapshot" "sample_snapshot" {
+
+#creating snapshot
+resource "aws_ebs_snapshot" "Sample_Snapshot" {
   volume_id = aws_ebs_volume.myVolume.id
   tags = {
     Name = "myEBS_Volsnap01"
-  }  
-}
-# 07.2 Display Snapshot id
-output "myout_snap_id" {
-  value = aws_ebs_snapshot.sample_snapshot.id
+  }
 }
 
-# 08. Start Chrome --> PublicIP
-resource "null_resource" "nulllocal1" {
-  depends_on = [ null_resource.nullremote3 ]
-  provisioner "local-exec" {
-    command = "start chrome ${aws_instance.web01.public_ip}"
-  }
+output "myout_snap_id" {
+	value = aws_ebs_snapshot.Sample_Snapshot.id
+}
+
+resource "null_resource" "nulllocal1"  {
+  depends_on = [
+    null_resource.nullremote3,
+  ]
+	provisioner "local-exec" {
+	    command = "start chrome  ${aws_instance.web01.public_ip}"
+  	}
 }
